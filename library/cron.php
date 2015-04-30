8a9fb79b921f3d032a72b6918698b9807c5f51a9
Chris Lagasse <soben@outlook.com>
2015-04-30 20:36:48 +0000

    Fixed the CRON. Guarantees that it works... as long as CRON works, lol. the cURL call works just great.

--- library/cron.php
+++ library/cron.php
@@ -7,19 +7,18 @@
 	 *
 	 *		Assumes your Wordpress Core is installed in the Site Root by default. If that's not the case, you must correct line 20.
 	 *
-	 *		If you're unsure of the path to this file, un-comment line 17, and run this file in the browser
+	 *		If you're unsure of the path to this file, un-comment line 18, and run this file in the browser
 	 *			http://yourwebsite.com/wp-content/plugins/gravityforms-hubspot/library/cron.php
 	 */		
 
-	//	Suggested Usage:
-	//			0 */6 * * * /path/to/wp-content/plugins/gravityforms-hubspot/library/cron.php
+	// Suggested Usage (cURL)
+	//			0 */6 * * * /usr/bin/curl http://yourwebsite.com/path/to/wp-content/plugins/gravityforms-hubspot/library/cron.php
 
 	$path = dirname( __FILE__ );
 	// echo $path;
 
 	require_once( dirname( dirname( dirname( dirname( $path ) ) ) ) . '/wp-load.php' );
 
-	// Probably redundant, but hey, I want to make sure this runs :)
 	// Our internal Wordpress-powered CRON runs every 6 hours, IF a user visits. This one runs every time the CRON runs, regardless of users visiting.
 	bsdGFHubspot::refresh_oauth_token();
 	update_option('gf_bsdhubspot_last_validated', time());
